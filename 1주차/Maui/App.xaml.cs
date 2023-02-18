8522afe3007b8409a359b31e7a811558f83f6158
Rudals <2153rud@gmail.com>
2023-02-18 18:59:10 +0900

    1주차_GroupStudy - Maui 자료 추가
    1주차_GroupStudy - Maui 자료 추가

---
+++ 1주차/Maui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Notes;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell(); // MainPage의 마크업의 루트 태그가 굳이 Shell 이 아니어도 된다.
+	}
+}
---
+++ 1주차/Maui/MauiProgram.cs
@@ -0,0 +1,19 @@
+﻿namespace Notes;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp() // EntryPoint
+	{
+		var builder = MauiApp.CreateBuilder(); // MauiAppBuilder 초기화
+
+        builder
+			.UseMauiApp<App>()  // <> 괄호 안에 들어간 형식의 Application 을 사용한다
+            .ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		return builder.Build(); // MauiApp을 빌드한다.
+	}
+}
